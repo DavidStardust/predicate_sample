c16b94010c30332cc86fc1bdab573895af103f86
David Magalhães <david.magalhaes90@hotmail.com>
2019-10-29 10:33:19 -0300

    Implementação da Interface

---
+++ src/application/Program.java
@@ -0,0 +1,31 @@
+//Fazer um programa que, a partir de uma lista de produtos, remova da
+//lista somente aqueles cujo preço mínimo seja 100.
+
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import entities.Product;
+import util.ProductPredicate;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		List<Product> list = new ArrayList<>();
+		
+		list.add(new Product("Tv", 900.00));
+		list.add(new Product("Mouse", 50.00));
+		list.add(new Product("Tablet", 350.50));
+		list.add(new Product("HD Case", 80.90));
+		
+		//list.removeIf( p -> p.getPrice() >= 100); //Expressão lambda com predicado
+		list.removeIf(new ProductPredicate());
+		
+		for (Product p : list) {
+			System.out.println(p);
+		}
+	}
+}
